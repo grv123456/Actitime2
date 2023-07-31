34dad3bc4ce64aa30cd6daf64d07ec037e6b6580
grval <grval@Gaurav>
2023-07-31 16:10:53 +0530

    Commit 1

---
+++ Actitime2/src/test/java/sandeep/Hello.java
@@ -0,0 +1,13 @@
+package sandeep;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Hello {
+	@Test
+	public void create() 
+	{
+		Reporter.log("XYZ",true);
+	}
+
+}
